17d087f4245f29614eb09f19a43e8733d0c4189b
Aurimas Urbonas <aurimas@smartpartner.lt>
2021-02-08 20:11:25 +0200

    Initial commit

---
+++ metadata.php
@@ -0,0 +1,11 @@
+<?php
+$sMetadataVersion = '2.1';
+
+$aModule = [
+    'id' => 'smp.demomodule',
+    'title' => 'Smart partner demo module',
+    'description' => 'Smart partner demo module',
+    'version' => '1.0.0',
+    'authos' => 'UAB Smart Partner',
+    'email' => 'info@smartpartner.lt'
+];
